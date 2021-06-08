e61ccfb23e614761e3aca04fe9b56cd01c064a94
Himanshu <himanshu@Himanshus-MacBook-Air.local>
2021-06-08 15:38:35 +0530

    First commit

---
+++ currency-exchange-service/src/main/java/com/springbootpractice/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,33 @@
+package com.springbootpractice.currencyexchangeservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @Autowired
+    public CurrencyExchangeRepository currencyExchangeRepository;
+    @Autowired
+    private Environment environment;
+    @GetMapping("currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from,@PathVariable String to)
+    {
+       String port= environment.getProperty("local.server.port");
+        CurrencyExchange currencyExchange=currencyExchangeRepository.findByFromAndTo(from,to);
+        if(currencyExchange==null)
+        {
+            throw new RuntimeException("Unable to find data for "+from+" to "+to);
+        }
+        currencyExchange.setEnvironment(port);
+
+
+        return currencyExchange;
+
+    }
+}
